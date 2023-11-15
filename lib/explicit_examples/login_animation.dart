8afca3a311a9ebfbe1204860f290503059899e48
Irshad <irshadkhaleel.p@gmail.com>
2023-11-15 19:46:55 +0530

    list animation

--- lib/explicit_examples/login_animation.dart
+++ lib/explicit_examples/login_animation.dart
@@ -10,12 +10,21 @@ class LoginScreenAnimation extends StatefulWidget {
 class _LoginScreenAnimationState extends State<LoginScreenAnimation>
     with SingleTickerProviderStateMixin {
   late AnimationController controller;
-  late Animation<double> opacity;
+  late Animation<double> logoFadeAnimation;
+  late Animation<Offset> slideAnimation;
+  late Animation<double> textExpansionAnimation;
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(vsync: this);
-    opacity = Tween<double>(begin: 0, end: 1).animate(controller);
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1500));
+    logoFadeAnimation = Tween<double>(begin: 0, end: 1).animate(controller);
+    slideAnimation = Tween(begin: const Offset(-1, -1), end: Offset.zero)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.ease));
+    textExpansionAnimation =
+        Tween<double>(begin: 0, end: 1).animate(controller);
+
+    controller.forward();
   }
 
   @override
@@ -25,31 +34,40 @@ class _LoginScreenAnimationState extends State<LoginScreenAnimation>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const FlutterLogo(
-              size: 100.0,
+            FadeTransition(
+              opacity: logoFadeAnimation,
+              child: const FlutterLogo(
+                size: 100.0,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                children: [
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Username',
-                    ),
+              child: SlideTransition(
+                position: slideAnimation,
+                child: ScaleTransition(
+                  scale: textExpansionAnimation,
+                  child: Column(
+                    children: [
+                      const TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      const TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Login'),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 16.0),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                    ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Login'),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
